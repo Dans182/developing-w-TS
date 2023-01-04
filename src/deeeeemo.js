7ac25833b7db4bdfb70703a1a5ce869b82e49bbd
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-04 18:18:56 +0100

    tsconfig.json configurado

--- src/deeeeemo.js
+++ src/deeeeemo.js
@@ -1,6 +1,19 @@
 //@ts-check
 //Uso el ts-check, para que usando ts me analice el fichero javascript
 
+//typescript transpila y transforma los archivos ts a js
+
+//npx tsc --init
+//Con este comando generamos automáticamente nuestro archivo TSConfig.json
+
+//npx tsc
+//Este comando es para transpilar todos los archivos
+
+//npx tsc --watch
+//Este comando es para que me haga una transpilación de manera reactiva, cada vez que guarde un archivo .ts que
+//hubiera sufrido cambios, acá me transpila todo a archivos .js
+
+
 (async ()=> {
   const myCart = [];
   const products = [];
