eea763982f22220bde3eae361c5b7c1b825a7f16
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-04 20:01:19 +0100

    add 01-hello.ts función anónima autoejecutable

--- src/01-hello.ts
+++ src/01-hello.ts
@@ -1,5 +1,7 @@
 /*const myName: string = "Daniel"
 let/const + La declaración o nombre de la variable + Tipado o tipo de valor +  valor*/
 
-const myName: string = 'Daniel';
-console.log(myName);
+(() => {
+  const myName: string = 'Daniel';
+  console.log(myName);
+})();
