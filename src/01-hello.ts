09cf059ef74c8dc1b7ec459caf915afd4c5e3ff1
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-04 19:38:27 +0100

    add 03-typo.ts

--- src/01-hello.ts
+++ src/01-hello.ts
@@ -1,2 +1,5 @@
-const myName = "Daniel";
+/*const myName: string = "Daniel"
+let/const + La declaración o nombre de la variable + Tipado o tipo de valor +  valor*/
+
+const myName: string = 'Daniel';
 console.log(myName);
--- src/02-demo2.ts
+++ src/02-demo2.ts
@@ -1,1 +1,1 @@
-const numbers = [1,3,4];
+const numbers = [1, 3, 4];
