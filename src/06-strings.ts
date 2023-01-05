852bb6fa191540846aac846ebd4527517ebadf7c
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-05 11:59:37 +0100

    add 06-STRINGS.ts

---
+++ src/06-strings.ts
@@ -0,0 +1,8 @@
+(() => {
+  let productTitle = 'My amazing product';
+  // productTitle = null;
+  // productTitle = () => {};
+  // productTitle = 123;
+  productTitle = 'My amazing product changed';
+  console.log('productTitle', productTitle);
+})();
