f1dd2c735443e29955de5a0f794a3b4f7b04bcb8
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-05 12:52:03 +0100

    add 06-STRINGS.ts v2

--- src/06-strings.ts
+++ src/06-strings.ts
@@ -5,4 +5,12 @@
   // productTitle = 123;
   productTitle = 'My amazing product changed';
   console.log('productTitle', productTitle);
+
+  const productDescription = "I'm bla bla bla bla";
+  console.log('productDescription', productDescription);
+
+  const summary = `Hello
+  my name
+  is
+  Daniel`;
 })();
