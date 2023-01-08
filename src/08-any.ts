5e2b4750cfdb910b0366016eed7ec12c2849e50a
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-08 16:41:37 +0100

    08-any.ts v3

--- src/08-any.ts
+++ src/08-any.ts
@@ -9,11 +9,19 @@
   //esa variable en particular.
   //Se aconseja que any no se use.
 
+  /*El any suele ser una buena herramienta para una migración hacia typescript,
+de manera que no sea radical*/
+
   myDinamicVar = 'Hola';
+  //al ser any, automaticamente ts ni js no nos ayuda ni nos indica que metodos se pueden usar
+  //para un string
   const rta = (myDinamicVar as string).toLowerCase();
+  //Haciendo un cast, obligo a que una varible sea tratada como un determinado estilo de dato.
   console.log(rta);
 
   myDinamicVar = 123456;
+  //Esta es otra forma de hacer un cast, para que una variable sea tratada de una determinada forma para obtener las ayudas sobre métodos y demás de ts.
+  //Son formas para que una variable sea tratada forzosamente como un tipo de dato que le indiques.
   const rta2 = (<number>myDinamicVar).toFixed();
   console.log(rta2);
 })();
