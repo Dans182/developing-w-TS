a49c4df62acc719413c8476aeb317d0a3d85d311
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-09 22:55:49 +0100

    10-alias.ts

--- src/10-alias.ts
+++ src/10-alias.ts
@@ -1,1 +1,16 @@
-(() => {})();
+(() => {
+  //type es una palabra de reservada de TS y a través de su uso puedo crear tipados personalizados.
+  type userID = string | number | boolean;
+
+  let userId: userID;
+
+  function greeting(userId: userID) {
+    if (typeof userId === 'string') {
+      console.log('string ' + userId.toLowerCase());
+    } else {
+    }
+  }
+
+  greeting('Prueba');
+  greeting(123);
+})();
