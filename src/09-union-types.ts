aff20e5e8b5fd94ba2019308d2e0813b38c24693
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-09 21:54:18 +0100

    09 listo

--- src/09-union-types.ts
+++ src/09-union-types.ts
@@ -1,16 +1,18 @@
-() => {
+(() => {
   let userId: string | number;
   userId = 123;
   userId = 'asasa';
 
   function greeting(myText: string | number) {
     if (typeof myText === 'string') {
-      console.log('string' + myText.toLowerCase());
+      console.log('string ' + myText.toLowerCase());
     } else {
       myText.toFixed();
-      console.log('number' + myText.toFixed(1));
+      console.log('number ' + myText.toFixed(1));
     }
   }
-  greeting(125);
+
   greeting('Prueba');
-};
+  greeting(123);
+
+})();
