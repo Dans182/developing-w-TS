2d108cfaa786dc6acbb1791118c6330238944f91
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-09 23:04:43 +0100

    10-alias.ts

--- src/10-alias.ts
+++ src/10-alias.ts
@@ -4,13 +4,20 @@
 
   let userId: userID;
 
-  function greeting(userId: userID) {
+  //Literal types
+  //Acá especifico que solo acepta una de las 5 opciones
+  type sizes = 'S' | 'M' | 'L' | 'XL' | 'XXL';
+  let shirtSize: sizes;
+  shirtSize = 'M';
+  shirtSize = 'L';
+  //let shirtSize: 'S' | 'M' | 'L' | 'XL' | 'XXL';
+  //shirtSize = 'Z';
+
+  function greeting(userId: userID, size: sizes) {
     if (typeof userId === 'string') {
       console.log('string ' + userId.toLowerCase());
     } else {
     }
   }
-
-  greeting('Prueba');
-  greeting(123);
+  greeting(11111, 'L');
 })();
