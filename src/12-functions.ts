974a4fb260d938e5c1b44eaccb927767dcb8f91e
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-09 23:58:49 +0100

    12-functions.ts

--- src/12-functions.ts
+++ src/12-functions.ts
@@ -1,1 +1,25 @@
-(() => {})();
+(() => {
+  type sizes = 'S' | 'M' | 'L' | 'XL' | 'XXL';
+
+  function createProductToJson(
+    title: string,
+    createdAt: Date,
+    stock: number,
+    size: sizes
+  ) {
+    return { title, createdAt, stock, size };
+  }
+  const producto1 = createProductToJson('P1', new Date(), 12, 'M');
+  console.log(producto1);
+  console.log(producto1.createdAt);
+  console.log(producto1.title);
+
+  const createProductToJsonV2 = (
+    title: string,
+    createdAt: Date,
+    stock: number,
+    size: sizes
+  ) => {
+    return { title, createdAt, stock, size };
+  };
+})();
