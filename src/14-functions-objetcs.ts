13c7c68cd33756ec1524ecd6ffe44ed72fe9e02f
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-11 00:55:49 +0100

    14- functions-objects

--- src/14-functions-objetcs.ts
+++ src/14-functions-objetcs.ts
@@ -29,4 +29,12 @@
     createdAd: new Date(2010, 1, 1),
     stock: 12,
   });
+
+  addProduct({
+    title: 'Prod2',
+    createdAd: new Date(1990, 31, 3),
+    stock: 18,
+    size: 'L',
+  });
+  console.log(products);
 })();
