d80918c1050da7dc5cf6ec786a9cf36d2e80c9ae
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-11 00:48:59 +0100

    14- functions-objects

--- src/14-functions-objetcs.ts
+++ src/14-functions-objetcs.ts
@@ -1,6 +1,15 @@
 (() => {
   const login = (email: string, password: string) => {
-    console.log(email, login);
+    console.log(email, password);
   };
   login('dans@gmail.com', '123121312');
+
+  //acá solo paso email y password, pero si realmente pasara muchos mas parametros, lo más comodo sería
+  //hacer uso de objetos.
+
+  //Acá abajo transformamos la primer función a formato "objeto".
+  const login2 = (data: { email: string; password: number }) => {
+    console.log(data.email, data.password);
+  };
+  login2({ email: 'dans@gmail.com', password: 123121312 });
 })();
