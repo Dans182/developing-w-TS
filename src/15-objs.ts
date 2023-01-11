60aef5948de56c60f1b629aafbe03758b7e94091
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-01-12 00:52:24 +0100

    indentado archivo 15

--- src/15-objs.ts
+++ src/15-objs.ts
@@ -1,16 +1,16 @@
 (() => {
-    //Acá aprendemos directamente a tipear objetos.
-  type sizes = "S" | "M" | "L" | "XL" | "XXL";
- 
- //Creamos nuestro objeto tipado
+  //Acá aprendemos directamente a tipear objetos.
+  type sizes = 'S' | 'M' | 'L' | 'XL' | 'XXL';
+
+  //Creamos nuestro objeto tipado
   type product = {
     title: string;
     createdAd: Date;
     stock: number;
     size?: sizes;
   };
-//indicamos que no puede recibir cualquier tipo de dato, quitamos el "any" y
-//le indicamos que solo puede recibir un objeto con las caracteristicas indicadas.
+  //indicamos que no puede recibir cualquier tipo de dato, quitamos el "any" y
+  //le indicamos que solo puede recibir un objeto con las caracteristicas indicadas.
   const products: product[] = [];
 
   const addProduct = (data: product) => {
@@ -18,27 +18,26 @@
   };
 
   addProduct({
-    title: "Prod1",
+    title: 'Prod1',
     createdAd: new Date(2010, 1, 1),
     stock: 12,
   });
 
   addProduct({
-    title: "Prod2",
+    title: 'Prod2',
     createdAd: new Date(1990, 31, 3),
     stock: 18,
-    size: "L",
+    size: 'L',
   });
   console.log(products);
   products.push({
-    title: "Prod3",
+    title: 'Prod3',
     createdAd: new Date(2090, 31, 3),
     stock: 88,
-    size: "L",
+    size: 'L',
   });
 
-
-/*En TypeScript también podemos usar los Alias para definir la estructura de tipado que debería tener un objeto:
+  /*En TypeScript también podemos usar los Alias para definir la estructura de tipado que debería tener un objeto:
 type userData = {
   username: string,
    email: string
@@ -59,5 +58,4 @@ usersList.push({
   username: "cvander", //CORRECTO
   email: true, // ERROR. Debe ser de tipo string y NO de tipo boolean
 });*/
-
 })();
--- src/products/products.model.ts
+++ src/products/products.model.ts
@@ -1,3 +1,5 @@
 //.model en TS no significa nada, es una forma visual para indicar que ese archivo tiene modelado modular
 //.service en TS es una forma visual para indicar que trata datos
 
+type sizes = 'S' | 'M' | 'L' | 'XL' | 'XXL';
+type product = { title: string; createdAd: Date; stock: number; size?: sizes };
